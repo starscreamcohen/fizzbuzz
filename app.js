929d1fced41e297175feff2173f2879a0f70c754
Benjamin Cohen <benjamin.cohen86@gmail.com>
2014-06-02 17:43:53 -0400

    adds javascript so that game is dynamically being played by users

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
-$(document).ready(function(){
 
-for(var i = 1; i < 100; i++) {
-	if(i % 3 == 0 && i % 5 == 0) {
-		$("body").append("<p>FizzBuzz</p>");
-	}
-	else if (i % 5 == 0) {
-		$("body").append("<p>Buzz</p>");
-	}
-	else if (i % 3 == 0){
-		$("body").append("<p>Fizz</p>");
+$(document).ready(function() {
+	setBindingUp();
+});
+
+function setBindingUp() {
+	$('#submit').on('click', function() {
+		myInputNumber();
+	});
+};
+
+
+
+
+var myInputNumber = function () {
+	var inputArea = $('#myInput').val();
+	if (inputArea.match(/[0-9]/)) {
+		appendFizzBuzz();
 	}
 	else {
-		$("body").append("<p>" + i + "</p>");
+		alert("That's Not How You Play!");
 	}
-}
 
-});
+};
+
+function appendFizzBuzz() {
+	var inputArea = $('#myInput').val();
+	for (var i = 1; i <= inputArea; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			$("#attach-game").append("<p>FizzBuzz</p>");
+		}
+		else if ( i % 5 === 0) {
+			$("#attach-game").append("<p>Buzz</p>");
+		}
+		else if (i % 3 === 0) {
+			$("#attach-game").append("<p>Fizz</p>");
+		}
+		else {
+			$("#attach-game").append("<p>" + i  + "</p>");
+		}	
+	}
+};
+
