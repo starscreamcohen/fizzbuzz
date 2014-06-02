a9a819bd1676130f70598276f1e9f38d53f7cdfb
Benjamin Cohen <benjamin.cohen86@gmail.com>
2014-06-02 19:38:33 -0400

    adds reset, focus and conditional statement for numbers over 100

--- app.js
+++ app.js
@@ -7,37 +7,57 @@ function setBindingUp() {
 	$('#submit').on('click', function() {
 		myInputNumber();
 	});
+
+	$('#myInput').on('keyup', function() {
+		if (event.keyCode==13) {
+			myInputNumber();
+		}
+	});
+
+	$('#playAgain').on('click', function() {
+		clearFizzBuzzGame();	
+	});
 };
-
-
 
 
 var myInputNumber = function () {
 	var inputArea = $('#myInput').val();
-	if (inputArea.match(/[0-9]/)) {
+	if (inputArea.match(/^\d+$/)) {
 		appendFizzBuzz();
 	}
 	else {
 		alert("That's Not How You Play!");
 	}
 
+	document.getElementById('myInput').value='';
 };
 
 function appendFizzBuzz() {
 	var inputArea = $('#myInput').val();
-	for (var i = 1; i <= inputArea; i++) {
-		if (i % 3 === 0 && i % 5 === 0) {
-			$("#attach-game").append("<p>FizzBuzz</p>");
+	if (inputArea <= 100) {
+		for (var i = 1; i <= inputArea; i++) {
+			if (i % 3 === 0 && i % 5 === 0) {
+				$("#attach-game").append("<p>FizzBuzz</p>");
+			}
+			else if ( i % 5 === 0) {
+				$("#attach-game").append("<p>Buzz</p>");
+			}
+			else if (i % 3 === 0) {
+				$("#attach-game").append("<p>Fizz</p>");
+			}
+			else {
+				$("#attach-game").append("<p>" + i  + "</p>");
+			}
+			
 		}
-		else if ( i % 5 === 0) {
-			$("#attach-game").append("<p>Buzz</p>");
-		}
-		else if (i % 3 === 0) {
-			$("#attach-game").append("<p>Fizz</p>");
-		}
-		else {
-			$("#attach-game").append("<p>" + i  + "</p>");
-		}	
 	}
+		else {
+			alert("Much Too High For This Game!");
+		}
+	
+};
+
+function clearFizzBuzzGame () {
+	$('p').remove();
 };
 
