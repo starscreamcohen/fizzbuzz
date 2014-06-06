77ba5347f858f7ad6a55fbd70efac10e85823210
Benjamin Cohen <benjamin.cohen86@gmail.com>
2014-06-06 15:24:29 -0400

    adds styling to application

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,31 +8,29 @@ function setBindingUp() {
 		testInputNumber();
 	});
 
-	$('#myInput').on('keyup', function() {
+	$('#myinput').on('keyup', function() {
 		if (event.keyCode==13) {
 			testInputNumber();
 		}
 	});
 
-	$('#playAgain').on('click', function() {
+	$('#playagain').on('click', function() {
 		clearFizzBuzzGame();	
 	});
 };
 
 function setUpAlerts () {
 	bootbox.alert(message, function() {
-
 	});
 
 	bootbox.confirm(message, function() {
-
 	});
 }
 
 
-
-var testInputNumber = function() {
-	var inputarea = $('#myInput').val(); 
+var inputarea;
+ function testInputNumber() {
+	 inputarea = $('#myinput').val();
 	if (inputarea.match(/^\d+$/)){ 
 		playFizzBuzz();
 		}	
@@ -40,35 +38,49 @@ var testInputNumber = function() {
 			bootbox.alert("That's Not How You Play!");
 		}
 	
-	document.getElementById('myInput').value='';
-
-
-	function playFizzBuzz() {
-		if (inputarea <= 100) {
-			for (var i = 1; i <= inputarea; i++) {
-				if (i % 3 === 0 && i % 5 === 0) {
-					$("#attach-game").append("<p>FizzBuzz</p>");
-				}
-				else if ( i % 5 === 0) {
-					$("#attach-game").append("<p>Buzz</p>");
-				}
-				else if (i % 3 === 0) {
-					$("#attach-game").append("<p>Fizz</p>");
-				}
-				else {
-					$("#attach-game").append("<p>" + i  + "</p>");
-				}
-				
-			}
-		}
-			else {
-				bootbox.alert("Much Too High For This Game!");
-			}
-	};
+	document.getElementById('myinput').value='';
 };
+// Blue For Fizz
+// Red For Buzz
+function addColorForFizzBuzz() {
+	if (String.match(/^Fizz$/)) {
+
+	}
+}
+function playFizzBuzz() {
+	if (inputarea <= 100) {
+		var attachGame = $("#attach-game");
+		if (attachGame) {
+		for (var i = 1; i <= inputarea; i++) {
+			if (i % 3 === 0 && i % 5 === 0) {
+				attachGame.append("<p><span>Fizz</span>" + "<span>Buzz</span></p>");
+			}
+			else if ( i % 5 === 0) {
+				attachGame.append("<p>Buzz</p>");
+			}
+			else if (i % 3 === 0) {
+				attachGame.append("<p>Fizz</p>");
+			}
+			else {
+				attachGame.append("<p>" + i  + "</p>");
+			}
+			
+		}
+	}
+	else {
+		console.log("attachGame is Null");
+	}
+	}
+		else {
+			bootbox.alert("Much Too High For This Game!");
+		}
+};
+
+
 
 
 function clearFizzBuzzGame () {
 	$('p').remove();
 };
+
 
